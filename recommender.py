8506c56d4dd6a61ea3b58d0a008ef37b9cefd389
Pulkit Gopalani <48027960+Pulkit-Go@users.noreply.github.com>
2019-03-22 11:12:40 +0530

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -7,63 +7,6 @@ from numpy import genfromtxt
 
 np.set_printoptions(threshold=np.inf)
 
-
-"""def normalize(data,nusers,nsongs):
-    datan=[[0 for x in range(nsongs)] for y in range(nusers)]
-    for i in range(nusers):
-        s=sum(data[i])
-        avg=s[0]/s[1]
-        for j in range(nsongs):
-            if(data[i][j]!=0):
-                datan[i][j]=data[i][j]-avg
-    return datan
-
-
-
-def calcSimilarity(datan,user,similarity,nusers,nsongs):
-    for i in range(nusers):
-        if(similarity[i][user]==0):
-            dotProd=0
-            for j in range(nsongs):
-                dotProd=dotProd+datan[user][j]*datan[i][j]
-            similarity[i][user]=similarity[user][i]=dotProd/(mod(i,nsongs)*mod(user,nsongs))
-            
-
-def findSimilarUsers(data, datan, nusers, similarity, user, song):
-    topMatch=[[0 for x in range(10)] for y in range(2)] #1st row contains similarity and second contains song rating of corresponding user
-    usedUsers=[0 for x in range(nusers)]
-    for i in range(10):
-        b=0  #current biggest similarity. we want only similarities>0 to be considered
-        ind=0 #user index of current biggest entry
-        f=0 #flag variable: does another similar user exist??
-        for j in range(nusers):
-            if(data[j][song]!=0 and similarity[user][j]>b and usedUsers[j]==0):
-                b=similarity[user][j]
-                ind=j
-                f=1
-        if(f==0):
-            break
-        usedUsers[ind]=1
-        topMatch[0][i]=b
-        topMatch[1][i]=datan[ind][song]
-    return topMatch
-"""
-def sum(row):
-    s=[0,0]   #s[0] stores sum of elements and s[1] stores no. of non zero elements
-    for i in row:
-        s[0]=s[0]+i
-        if i!=0:
-            s[1]=s[1]+1
-    return s
-
-
-def mod(i,data,nsongs):
-    m=0
-    for j in range(nsongs):
-        m=m+data[i][j]*data[i][j]
-    m=math.sqrt(m)
-    return m
-
 def predict(topMatch,data,song):
     i=0#current index
     s=0#sum of similarities
@@ -74,11 +17,9 @@ def predict(topMatch,data,song):
         i=i+1
     prediction=dotProd/s
     return prediction
+    
 def calculateSimilarity(a,b,data):
-    dotProd=0
-    for j in range(nsongs):
-        dotProd=dotProd+data[a][j]*data[b][j]
-    return dotProd/(mod(a,data,nsongs)*mod(b,data,nsongs))
+	return np.dot(data[a],data[b])/math.sqrt( np.sum(np.square(data[a])) * np.sum(np.square(data[b])) )
 
 def match(user,s,noMatch,topMatch):
     for i in range(noMatch):
@@ -89,18 +30,13 @@ def match(user,s,noMatch,topMatch):
 
 #0 is considered as unrated
 data = genfromtxt("dummyData.csv", delimiter=',')	#numpy 2D array stores data
-
-                 
-                 
+                
 nusers=data.shape[0]  #no. of users
 nsongs=data.shape[1]   #no. of songs
 
 
-#datan=normalize(data,nusers,nsongs) #normalized data set
-
-#similarity=[[0 for x in range(nusers)] for y in range(nusers)]
-
 datacalc=[[0 for x in range(nsongs)] for y in range(nusers)]    #calculated data
+#datacalc=np.array(datac)
 
 for i in range(nusers):
     #f=0  #flag: is similarity calculated??
@@ -111,12 +47,9 @@ for i in range(nusers):
         match(k,s,noMatch,topMatch)
     for j in range(nsongs):
         if (data[i][j]==0):
-            """if(f==0):
-                calcSimilarity(datan,i,similarity,nusers,nsongs)
-                f=1
-            topMatch=findSimilarUsers(data,datan,nusers,similarity,i,j)"""
-            s=sum(data[i])
-            avg=s[0]/s[1]
+            
+            
+            avg=np.sum(data[i])/np.count_nonzero(data[i])
             datacalc[i][j]=round(predict(topMatch,data,j)+avg,3)
 
 nrecc=5
@@ -127,6 +60,7 @@ dataSort=np.ones(shape=(nusers,nrecc+1))*-1
 
 for i in range(0,nrecc+1):
 	dataSort[0][i]=i
+
 
 
 for i in range(1,nusers):
